b26920f8ad93172c54422ad40d85dc3678417939
Matěj Sychra <suculent@me.com>
2017-08-16 11:17:39 +0200

    verified intergration; build artifacts are now stored in the /build folder

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN apt-get update && apt-get install -y wget unzip git make python-serial \
  && python -m pip install --upgrade pip \
  && pip install -U platformio
 
-RUN mkdir /opt/platformio-builder
-WORKDIR /opt/platformio-builder
+RUN mkdir /opt/workspace
+WORKDIR /opt/workspace
 COPY cmd.sh /opt/
 CMD /opt/cmd.sh
--- cmd.sh
+++ cmd.sh
@@ -12,6 +12,40 @@ else
   true
 fi
 
-cd /opt/platformio-builder
+cd /opt/workspace
+
+#
+# Build
+#
 
 platformio run
+
+#
+# Export artefacts
+#
+
+if [[ -d build ]]; then
+  rm -rf build
+fi
+  mkdir build
+fi
+
+cd ./.pioenvs
+
+# WARNING! Currently supports only one simultaneous
+# build-environment and overwrites OUTFILE(s) with recents.
+
+for dir in $(ls -d */); do
+  echo $dir
+  if [[ -d $dir ]]; then
+    echo "DIR: $dir"
+    pushd $dir
+    if [[ -f firmware.bin ]]; then
+      cp firmware.bin /opt/workspace/build/firmware.bin
+    fi
+    if [[ -f firmware.elf ]]; then
+      cp firmware.elf /opt/workspace/build/firmware.elf
+    fi
+    popd
+  fi
+done
